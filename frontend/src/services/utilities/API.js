299aad3f2ac59638beec000b817c15afd70f32fb
Farhan Ghazi <farhansghazi@outlook.com>
2020-09-04 18:00:59 +1000

    added new API utility

--- frontend/src/services/utilities/API.js
+++ frontend/src/services/utilities/API.js
@@ -1,6 +1,7 @@
 import APICore from './Core';
 
 const storiesURL = 'stories';
+const tracksURL = 'tracks';
 
 export const apiStories = new APICore({
     getAll: true,
@@ -10,6 +11,16 @@ export const apiStories = new APICore({
     patch: true,
     remove: true,
     url: storiesURL,
+})
+
+export const apiTracks = new APICore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: true,
+    remove: true,
+    url: tracksURL,
 })
 
 export default apiStories;
