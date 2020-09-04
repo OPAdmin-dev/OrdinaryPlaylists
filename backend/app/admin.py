ed41de81d7da75ac1a518a574c15fd041b38af6b
Farhan Ghazi <farhansghazi@outlook.com>
2020-09-05 00:54:45 +1000

    register Tags in admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Stories, Tracks, TrackImage
+from .models import Stories, Tracks, TrackImage, Tags
 
 admin.site.register(Stories)
 admin.site.register(Tracks)
 admin.site.register(TrackImage)
+admin.site.register(Tags)
