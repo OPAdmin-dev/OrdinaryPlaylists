8d0772ec7f09508facd8d3b76bd5a169d8205a58
Farhan Ghazi <farhansghazi@outlook.com>
2020-09-06 11:43:21 +1000

    hotfix: update table name in admin panel

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,6 +5,7 @@ class Stories(models.Model):
 
     class Meta:
         db_table = 'stories'
+        verbose_name_plural = "Stories"
 
     name = models.CharField(max_length=200, default="Anonymous")
     story = models.TextField(default="Story")
@@ -16,6 +17,7 @@ class TrackImage(models.Model):
 
     class Meta:
         db_table = 'track_images'
+        verbose_name_plural = "Track Images"
 
     track = models.ForeignKey('Tracks', on_delete=models.CASCADE, related_name="tracks")
     cover = models.ImageField(upload_to="images/", default="Track Cover")
@@ -24,6 +26,7 @@ class Tracks(models.Model):
 
     class Meta:
         db_table = 'tracks'
+        verbose_name_plural = "Tracks"
 
     story = models.ForeignKey(Stories, on_delete=models.CASCADE, related_name="stories")
     name = models.CharField(max_length=200, default="Track Name")
@@ -42,6 +45,7 @@ class Tags(models.Model):
 
     class Meta:
         db_table = 'Tags'
+        verbose_name_plural = "Tags"
 
     tag = models.CharField(max_length=200, default="Tag Name")
 
@@ -52,6 +56,7 @@ class Playlists(models.Model):
 
     class Meta:
         db_table = 'playlists'
+        verbose_name_plural = "Playlists"
 
     name = models.CharField(max_length=200, default="Playlist Name")
     description = models.TextField(default="Playlist Description")
