b9832cd897c9aaacdd133f5b47f0fc35be61f42c
Farhan Ghazi <farhansghazi@outlook.com>
2020-10-11 12:19:42 +1100

    use staticfiles

--- project/settings.py
+++ project/settings.py
@@ -171,7 +171,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
