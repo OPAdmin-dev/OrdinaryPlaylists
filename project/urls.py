7c4cec405f43d2eff65686b501da77da359c65b6
Farhan Ghazi <farhansghazi@outlook.com>
2020-10-13 10:37:11 +1100

    update urls

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls import url
 from django.conf.urls.static import static
 from project import settings
 from scripts.OPBot import runBot
@@ -29,7 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dev-only/', include('app.api.urls')),
     path('', include('app.urls')),
-    url(r'^', index)
+    path('', index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
