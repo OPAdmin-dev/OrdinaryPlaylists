7da53ce99388351a9a0b8e26890e49f17961d12c
gilbertzhuo <gilbertzhuo@gmail.com>
2020-10-12 18:59:49 +1100

    merge

---
+++ src/Components/NotFound404.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div className="NotFound">
+            <h1>404 PAGE NOT FOUND</h1>
+        </div>
+    )
+}
+export default PageNotFound;
