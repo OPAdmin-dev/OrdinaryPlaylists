ff30107c6dbadda650c6efbcd5dee041c4fd077a
Farhan Ghazi <farhansghazi@outlook.com>
2020-10-13 17:04:26 +1100

    update urls

--- project/urls.py
+++ project/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dev-only/', include('app.api.urls')),
     path('dev-only/spotify/', SpotifyClient, name='spotify'),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('.*', TemplateView.as_view(template_name='index.html')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
