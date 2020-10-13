526347b11720a391b85916ccf49751e9f543486d
Farhan Ghazi <farhansghazi@outlook.com>
2020-10-13 18:31:21 +1100

    update urls

--- project/urls.py
+++ project/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dev-only/', include('app.api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    url(r'^.*/', TemplateView.as_view(template_name="index.html")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
