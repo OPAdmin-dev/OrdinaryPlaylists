7097a4ae795167ba67049024d9f466e3b82696f6
Farhan Ghazi <farhansghazi@outlook.com>
2020-10-13 14:24:21 +1100

    update urls

--- app/views.py
+++ app/views.py
@@ -7,8 +7,6 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 import json
 
-index = never_cache(TemplateView.as_view(template_name='index.html'))
-
 
 def SpotifyClient(request):
     try:
