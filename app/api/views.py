ed87301541cb76526df50b358410c305bb5b97a1
Farhan Ghazi <farhansghazi@outlook.com>
2020-11-26 16:35:56 +1100

    update settings

--- app/api/views.py
+++ app/api/views.py
@@ -20,7 +20,6 @@ from rest_framework.response import Response
 from django.http import Http404
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from scripts.OPBot import runBot
 
 
 class StorySet(viewsets.ModelViewSet):
